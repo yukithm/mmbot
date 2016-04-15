e779b85e6f6e0d1a767f025317cb919fa3665452
Yuki <yukithm@gmail.com>
2016-04-15 13:52:08 +0900

    Add comment

--- route.go
+++ route.go
@@ -2,10 +2,20 @@ package mmbot
 
 import "net/http"
 
+// RouteHandlerFunc is route action function.
 type RouteHandlerFunc func(*Robot, http.ResponseWriter, *http.Request) error
 
+// Route is a HTTP route.
 type Route struct {
+	// HTTP method (e.g. "GET", "POST").
+	// All methods are allowed if empty.
 	Methods []string
+
+	// Route pattern (e.g. "/articles/{category}/{id:[0-9]+}")
+	// Pattern can have variables that are defined by "{name}" or "{name:regexp}" format.
+	// Variables can be retrieved calling Robot.RouteVars().
 	Pattern string
-	Action  RouteHandlerFunc
+
+	// Route action.
+	Action RouteHandlerFunc
 }
